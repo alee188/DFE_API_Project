5485d96f893cea006350b8242c8dac687f5252d2
alee188 <anthonylee188@yahoo.com>
2022-07-13 15:41:18 +0100

    Columns made

--- APIdir/DFECountryAPI/src/main/java/com/qa/dfespringboot/entities/Country.java
+++ APIdir/DFECountryAPI/src/main/java/com/qa/dfespringboot/entities/Country.java
@@ -1,5 +1,32 @@
 package com.qa.dfespringboot.entities;
 
-public class Country {
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
+@Entity
+public class Country {
+	@Id
+	@GeneratedValue(strategy = GenerationType. Identity)
+	private long id;
+	
+	@Column(name = "countryname", nullable = false)
+	private String countryName;
+	
+	@Column(nullable = false)
+	private String capitalCity;
+	
+	@Column(nullable = false)
+	private String officalLanguage;
+	
+	@Column(nullable = false)
+	private int populationSize;
+	
+	@Column(nullable = false) 
+	private String grossDomesticProduct;
+	
+	
+	
 }
