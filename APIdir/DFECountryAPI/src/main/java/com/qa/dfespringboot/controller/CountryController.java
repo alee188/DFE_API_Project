7c9ae983bfc792ec2e5b9cd967ecc766de129601
alee188 <anthonylee188@yahoo.com>
2022-07-13 17:56:10 +0100

    added create functionality

--- APIdir/DFECountryAPI/src/main/java/com/qa/dfespringboot/controller/CountryController.java
+++ APIdir/DFECountryAPI/src/main/java/com/qa/dfespringboot/controller/CountryController.java
@@ -3,6 +3,7 @@ package com.qa.dfespringboot.controller;
 import java.util.List;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,11 +13,19 @@ import com.qa.dfespringboot.entities.Country;
 @RestController
 @RequestMapping ("/country")// adds a prefix to the request URLs
 public class CountryController {
+
+// Create functionality
+	@PostMapping("/create")
+	public Country create(@RequestBody Country country) {
+		return this.service.create(Country);
+	}
 	
 // Read functionality
 	@GetMapping("/getALL")
 	public List <Country> getALL(){
 		return this.service.getALL();
 	}
+	
+	
 	
 }
