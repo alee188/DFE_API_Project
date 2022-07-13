183fb1c1246a00525f7913e547106d893ae69bc6
alee188 <anthonylee188@yahoo.com>
2022-07-13 19:06:08 +0100

    added read function to service

--- APIdir/DFECountryAPI/src/main/java/com/qa/dfespringboot/service/CountryService.java
+++ APIdir/DFECountryAPI/src/main/java/com/qa/dfespringboot/service/CountryService.java
@@ -1,5 +1,7 @@
 package com.qa.dfespringboot.service;
 
+import java.util.List;
+
 import org.springframework.stereotype.Service;
 
 import com.qa.dfespringboot.entities.Country;
@@ -10,8 +12,10 @@ public class CountryService {
 
 // create: add new country
 	public Country create (Country country) {
-		return this.repo.saveAndFlush(country);
-		
+		return this.repo.saveAndFlush(country);	
 	}
-	
+// read: return whole list
+	public List <Country> getALL(){
+		return this.repo.findAll();
+	} 
 }
