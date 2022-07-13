052787a7067de3a44c5ac586ea849188f57e789c
alee188 <anthonylee188@yahoo.com>
2022-07-13 20:31:10 +0100

    added the remainder of the imports

--- APIdir/DFECountryAPI/src/main/java/com/qa/dfespringboot/repo/CountryRepo.java
+++ APIdir/DFECountryAPI/src/main/java/com/qa/dfespringboot/repo/CountryRepo.java
@@ -1,8 +1,13 @@
 package com.qa.dfespringboot.repo;
 
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-@Repository
-public class CountryRepo {
+import com.qa.dfespringboot.entities.Country;
 
+@Repository
+public interface CountryRepo extends JpaRepository <Country, Long> {
+	List<Country> findCountrybyCountryName(String countryName);
 }
