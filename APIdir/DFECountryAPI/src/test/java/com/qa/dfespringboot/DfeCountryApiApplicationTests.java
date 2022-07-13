f9faf6227d875f15ab0804049ede7d05e3ce7d28
alee188 <anthonylee188@yahoo.com>
2022-07-13 14:09:18 +0100

    initialising local spring starter to the development branch

---
+++ APIdir/DFECountryAPI/src/test/java/com/qa/dfespringboot/DfeCountryApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.qa.dfespringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DfeCountryApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
