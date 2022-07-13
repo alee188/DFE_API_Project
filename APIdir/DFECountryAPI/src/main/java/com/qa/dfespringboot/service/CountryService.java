3d8bc12b3065114ad9391e07066cced494b04b5c
alee188 <anthonylee188@yahoo.com>
2022-07-13 19:03:08 +0100

    added create function to service

--- APIdir/DFECountryAPI/src/main/java/com/qa/dfespringboot/service/CountryService.java
+++ APIdir/DFECountryAPI/src/main/java/com/qa/dfespringboot/service/CountryService.java
@@ -1,6 +1,17 @@
 package com.qa.dfespringboot.service;
 
+import org.springframework.stereotype.Service;
+
+import com.qa.dfespringboot.entities.Country;
+
 @Service
 public class CountryService {
+//Service components are used to store the main business logic of a Spring Boot application 
 
+// create: add new country
+	public Country create (Country country) {
+		return this.repo.saveAndFlush(country);
+		
+	}
+	
 }
