c05bc578f52de7a650bc5fa17b048e863c0edd6b
alee188 <anthonylee188@yahoo.com>
2022-07-13 16:01:14 +0100

    added Hashcode

--- APIdir/DFECountryAPI/src/main/java/com/qa/dfespringboot/entities/Country.java
+++ APIdir/DFECountryAPI/src/main/java/com/qa/dfespringboot/entities/Country.java
@@ -1,5 +1,7 @@
 package com.qa.dfespringboot.entities;
 
+import java.util.Objects;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -86,6 +88,25 @@ public class Country {
 
 	public void setGrossDomesticProduct(String grossDomesticProduct) {
 		this.grossDomesticProduct = grossDomesticProduct;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(capitalCity, countryName, grossDomesticProduct, id, officialLanguage, populationSize);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Country other = (Country) obj;
+		return Objects.equals(capitalCity, other.capitalCity) && Objects.equals(countryName, other.countryName)
+				&& Objects.equals(grossDomesticProduct, other.grossDomesticProduct) && id == other.id
+				&& Objects.equals(officialLanguage, other.officialLanguage) && populationSize == other.populationSize;
 	}
 				
 	
