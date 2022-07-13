841513d8ed807883e79f0490f45b60ce0e5d32e3
alee188 <anthonylee188@yahoo.com>
2022-07-13 17:52:09 +0100

    added read functionality

--- APIdir/DFECountryAPI/src/main/java/com/qa/dfespringboot/controller/CountryController.java
+++ APIdir/DFECountryAPI/src/main/java/com/qa/dfespringboot/controller/CountryController.java
@@ -1,5 +1,22 @@
 package com.qa.dfespringboot.controller;
 
-public class CountryController {
+import java.util.List;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.qa.dfespringboot.entities.Country;
+
+// Handle incoming HTTP requests and send responses
+@RestController
+@RequestMapping ("/country")// adds a prefix to the request URLs
+public class CountryController {
+	
+// Read functionality
+	@GetMapping("/getALL")
+	public List <Country> getALL(){
+		return this.service.getALL();
+	}
+	
 }
