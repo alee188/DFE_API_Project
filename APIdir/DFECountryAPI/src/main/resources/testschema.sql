38543c2bd78aac94571e0c006007ab11c4b4ebb7
alee188 <anthonylee188@yahoo.com>
2022-07-14 19:04:32 +0100

    added testschema.sql to resource

--- APIdir/DFECountryAPI/src/main/resources/testschema.sql
+++ APIdir/DFECountryAPI/src/main/resources/testschema.sql
@@ -1,3 +1,12 @@
-DROP TABLE IF EXISTS 'Country';
+DROP TABLE IF EXISTS `Country`;
 
-CREATE TABLE 'Country' (
+CREATE TABLE `Country` (
+	`id` BIGINT AUTO_INCREMENT,
+	`countryname` VARCHAR(255) NOT NULL, 
+	`capital_city` VARCHAR(255) NOT NULL,
+	`official_language` VARCHAR(255) NOT NULL,
+	`population_size` INT NOT NULL,
+	`gross_domestic_product` VARCHAR(255) NOT NULL,
+	PRIMARY KEY(`id`)
+	
+); 
